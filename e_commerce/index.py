12d04c21c8ed063f7a768673140ad7ead50ba5c1
Lưu Việt Hoàng <luu186835@gmail.com>
2024-11-04 15:11:13 +0700

    Add initial data files and implement product listing functionality

--- e_commerce/index.py
+++ e_commerce/index.py
@@ -1,9 +1,17 @@
+from itertools import product
+
 from e_commerce import app
 from flask import render_template
+import utils
 @app.route('/')
 def home():
-    return render_template("index.html")
-
+    cates = utils.load_categories()
+    return render_template("index.html",
+                           categories=cates)
+@app.route('/products')
+def product_list():
+    products = utils.load_products()
+    return render_template("products.html",products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ e_commerce/utils.py
@@ -0,0 +1,13 @@
+import json, os
+from e_commerce import app
+
+
+def read_json(path):
+     with open(path, 'r') as f:
+        return json.load(f)
+
+def load_categories():
+    return read_json(os.path.join(app.root_path,'data/categories.json'))
+
+def load_products():
+    return read_json(os.path.join(app.root_path,'data/products.json'))
