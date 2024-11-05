318e74a0a99f3c4fa2525f1a4eb1ad8c7697b223
Lưu Việt Hoàng <luu186835@gmail.com>
2024-11-05 23:57:54 +0700

    Update .gitignore to exclude .env and configure database URI using environment variable

--- e_commerce/__init__.py
+++ e_commerce/__init__.py
@@ -1,8 +1,13 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/saleappDB?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app=app)
